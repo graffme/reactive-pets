5dbcb8bd47084597d5d694ac38eafe7a6a8171e6
daria <daria.rucka@gmail.com>
2018-10-10 20:35:31 +0200

    Updates

---
+++ precache-manifest.b64bb0f8c2adcd5a8c31d28d4f4bd723.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c97cf3e4fa63d740b6007913dd17e99",
+    "url": "/reactive-pets/static/media/paw.0c97cf3e.svg"
+  },
+  {
+    "revision": "d4adbe610a90689237bd",
+    "url": "/reactive-pets/static/js/runtime~main.d4adbe61.js"
+  },
+  {
+    "revision": "f4c06ae2b481123f3254",
+    "url": "/reactive-pets/static/js/main.f4c06ae2.chunk.js"
+  },
+  {
+    "revision": "f4661cbd474c5f7f0c2e",
+    "url": "/reactive-pets/static/js/1.f4661cbd.chunk.js"
+  },
+  {
+    "revision": "f4c06ae2b481123f3254",
+    "url": "/reactive-pets/static/css/main.f81ede36.chunk.css"
+  },
+  {
+    "revision": "f4661cbd474c5f7f0c2e",
+    "url": "/reactive-pets/static/css/1.dcf6dadf.chunk.css"
+  },
+  {
+    "revision": "e209af2845e799568a13f0897304bff9",
+    "url": "/reactive-pets/index.html"
+  }
+];
